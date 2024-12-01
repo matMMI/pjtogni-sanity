8d5ffe497efc24eab71d52b751656a28129c0f97
gitfanlab <mathis.togni@sfr.fr>
2024-12-01 12:59:37 +0100

    Maj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+
 const hankenGroteskBlack = localFont({
   src: "../../public/fonts/HankenGrotesk-Black.ttf",
   variable: "--font-hanken-black",
@@ -34,29 +34,64 @@ export default function RootLayout({
       className={`${hankenGroteskBlack.variable} ${hankenGroteskRegular.variable} ${hankenGroteskLight.variable}`}
     >
       <head>
-        <link rel="stylesheet" href="/favicon.ico" />
         <link
           rel="icon"
+          href="icons/favicon-16x16.png"
+          sizes="16x16"
           type="image/png"
-          sizes="32x32"
-          href="/icons/icon-32x32.png"
         />
         <link
           rel="icon"
+          href="icons/favicon-32x32.png"
+          sizes="32x32"
           type="image/png"
-          sizes="16x16"
-          href="/icons/icon-16x16.png"
         />
 
-        <link rel="apple-touch-icon" sizes="57x57" href="icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="icon-180x180.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/icons/icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/icons/icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icons/icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/icons/icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/icons/icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/icons/icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icons/icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/icon-180x180.png"
+        />
 
         <link rel="manifest" href="/manifest.json" />
       </head>
