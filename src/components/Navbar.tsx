a1d49af765c25e850699ac6fc892fe6be18ae374
gitfanlab <mathis.togni@sfr.fr>
2024-12-14 17:17:24 +0100

    Maj

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,57 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Menu, X } from "lucide-react";
+import { client } from "@/sanity/client";
+
+interface Resume {
+  pdfFile: {
+    asset: {
+      url: string;
+    };
+  };
+  active: boolean;
+}
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [resumeData, setResumeData] = useState<Resume | null>(null);
   const pathname = usePathname();
 
   const navLinks = [
     { href: "/a-propos", label: "À propos" },
     { href: "/projects", label: "Tous mes projets" },
-    // { href: "/test", label: "Test" },
   ];
+
+  useEffect(() => {
+    const fetchResume = async () => {
+      try {
+        const resume = await client.fetch<Resume | null>(`
+          *[_type == "resume" && active == true][0]{
+            "pdfFile": {
+              "asset": {
+                "url": pdfFile.asset->url
+              }
+            },
+            active
+          }
+        `);
+        setResumeData(resume);
+      } catch (error) {
+        console.error("Erreur lors de la récupération du CV:", error);
+      }
+    };
+
+    fetchResume();
+  }, []);
 
   const isActiveLink = (path: string) => pathname === path;
   const handleLinkClick = () => {
     setIsMobileMenuOpen(false);
   };
+
   return (
     <>
       <button
@@ -28,7 +61,6 @@ const Navbar = () => {
         {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
       </button>
 
-      {/* Navigation Bar */}
       <nav
         className={`fixed z-10 lg:relative flex-none w-[350px] h-screen bg-black text-white flex flex-col justify-between p-16 overflow-hidden transition-transform duration-300 ease-in-out ${
           isMobileMenuOpen
@@ -36,7 +68,6 @@ const Navbar = () => {
             : "-translate-x-full lg:translate-x-0"
         }`}
       >
-        {/* Logo Section */}
         <div className="relative z-10">
           <div className="text-center mb-20">
             <h1 className="text-2xl font-bold whitespace-nowrap">
@@ -46,13 +77,12 @@ const Navbar = () => {
             <p className="text-sm uppercase mt-2 opacity-80">Infographiste</p>
           </div>
 
-          {/* Navigation Links */}
           <ul className="space-y-6">
             {navLinks.map((link) => (
               <li key={link.href}>
                 <Link
                   href={link.href}
-                  onClick={handleLinkClick} // Ajout du gestionnaire d'événements
+                  onClick={handleLinkClick}
                   className={`block text-lg transition-all duration-200 hover:text-gray-300 relative group ${
                     isActiveLink(link.href) ? "text-white" : "text-gray-400"
                   }`}
@@ -71,17 +101,17 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="relative z-10">
-          <a
-            href="/cv.pdf"
-            download
-            className="inline-block px-6 py-3 border border-white text-white transition-all duration-300 hover:bg-white hover:text-black hover:shadow-lg transform hover:-translate-y-1"
-          >
-            Télécharger le CV
-          </a>
-        </div>
-
-        {/* Background Logo */}
+        {resumeData?.pdfFile?.asset?.url && (
+          <div className="relative z-10">
+            <a
+              href={resumeData.pdfFile.asset.url}
+              download
+              className="inline-block px-6 py-3 border border-white text-white transition-all duration-300 hover:bg-white hover:text-black hover:shadow-lg transform hover:-translate-y-1"
+            >
+              Télécharger le CV
+            </a>
+          </div>
+        )}
 
         <div className="absolute bottom-10 left-1/2 -translate-x-1/2 w-[540px] h-[510px] bg-[url('/logo.svg')] bg-no-repeat bg-center bg-contain opacity-60" />
       </nav>
